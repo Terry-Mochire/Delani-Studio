888211c12dc7489870fce41d1571eae0669530c8
Terry <terrybmochire@gmail.com>
2022-03-12 14:29:11 +0300

    write jQuery script to toggle what we do section.

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#design').click(function(){
+        $('#design img').toggle();
+        $('.design p').toggle();
+    });
+    $('#development').click(function(){
+        $('#development img').toggle();
+        $('.development p').toggle();
+    });
+    $('#product').click(function(){
+        $('#product img').toggle();
+        $('.product p').toggle();
+    });
+
+});
